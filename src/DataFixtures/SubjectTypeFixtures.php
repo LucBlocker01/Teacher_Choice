0d82de5dba2da9a648cb6f2adfd18257c9eb093d
Baptiste Clochette <baptiste.clochette@etudiant.univ-reims.fr>
2023-09-26 16:06:26 +0200

    feat: creation of the fake data of SubjectType

--- src/DataFixtures/SubjectTypeFixtures.php
+++ src/DataFixtures/SubjectTypeFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\SubjectTypeFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,9 @@ class SubjectTypeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        SubjectTypeFactory::createOne(['name' => 'CM']);
+        SubjectTypeFactory::createOne(['name' => 'TD']);
+        SubjectTypeFactory::createOne(['name' => 'TDM']);
+        SubjectTypeFactory::createOne(['name' => 'TP']);
     }
 }
