81ed7774eeae17d7cf6b2abf5dbeb1a7c7ce4737
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-09-27 14:13:57 +0200

    fix: fixtures for users: Status table on BD

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,13 +4,21 @@ namespace App\DataFixtures;
 
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         UserFactory::createOne(['login' => 'admin', 'password' => 'test']);
         UserFactory::createMany(3);
+    }
+
+    public function getDependencies()
+    {
+        return [
+          StatusFixtures::class,
+        ];
     }
 }
