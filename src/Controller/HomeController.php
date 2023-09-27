d158df64fc8cd7349a59b2043cf0b63cfa9e2259
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-09-27 16:06:43 +0200

    fix: remove the isGranted avoid instant redirection login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,14 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
     }
 
     #[Route('/react', name: 'app_react')]
-    #[IsGranted('ROLE_USER')]
     public function react(): Response
     {
         return $this->render('home/react.html.twig');
