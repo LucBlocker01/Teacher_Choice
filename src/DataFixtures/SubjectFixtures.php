e3daff7312e8dd5b7e50440ee010fb8712d293c6
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-09-27 12:09:52 +0200

    feat: update generation of Subjects

--- src/DataFixtures/SubjectFixtures.php
+++ src/DataFixtures/SubjectFixtures.php
@@ -3,7 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Factory\SubjectFactory;
-use App\Factory\SubjectTypeFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -12,32 +11,10 @@ class SubjectFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        SubjectFactory::createMany(40, function () {
-            $randomNumber = rand(1, 4);
-            if (1 == $randomNumber) {
-                return [
-                    'nb_group' => 1,
-                    'type' => SubjectTypeFactory::find(['id' => 1]),
-                ];
-            }
-            if (2 == $randomNumber) {
-                return [
-                    'nb_group' => 2,
-                    'type' => SubjectTypeFactory::find(['id' => 2]),
-                ];
-            }
-            if (3 == $randomNumber) {
-                return [
-                    'nb_group' => 2,
-                    'type' => SubjectTypeFactory::find(['id' => 3]),
-                ];
-            } else {
-                return [
-                    'nb_group' => 4,
-                    'type' => SubjectTypeFactory::find(['id' => 4]),
-                ];
-            }
-        });
+        $json = json_decode(file_get_contents(__DIR__.'/data/Subject.json'), true);
+        foreach ($json as $element) {
+            SubjectFactory::createOne($element);
+        }
     }
 
     public function getDependencies(): array
