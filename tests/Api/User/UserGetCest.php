26c746acf639240653a36cb03597d5db037fcca3
Erwann Maton <erwann.mutant@gmail.com>
2023-09-29 15:24:02 +0200

    test: user get cest test valid

--- tests/Api/User/UserGetCest.php
+++ tests/Api/User/UserGetCest.php
@@ -2,6 +2,10 @@
 
 namespace App\Tests\Api\User;
 
+use App\Entity\User;
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+
 class UserGetCest
 {
     protected static function expectedProperties(): array
@@ -10,6 +14,26 @@ class UserGetCest
             'id' => 'integer',
             'login' => 'string',
             'roles' => 'array',
+            'lastname' => 'string',
+            'firstname' => 'string',
         ];
+    }
+
+    public function anonymousUserGetSimpleUserElement(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        $data = [
+            'login' => 'user1',
+        ];
+        UserFactory::createOne($data);
+
+        // 2. 'Act'
+        $I->sendGet('/api/users/1');
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(User::class, '/api/users/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), $data);
     }
 }
