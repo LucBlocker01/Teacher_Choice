37cba4558cbde5a6c332f36cae4b39d138711d71
Erwann Maton <erwann.mutant@gmail.com>
2023-09-29 15:23:26 +0200

    feat: add groups to roles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -66,6 +66,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $login = null;
 
     #[ORM\Column]
+    #[Groups(['get_Me', 'get_User'])]
     private array $roles = [];
 
     /**
