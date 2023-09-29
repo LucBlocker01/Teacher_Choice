34effbe4a97e03ec3a41f9d5802d5900833927a9
Erwann Maton <erwann.mutant@gmail.com>
2023-09-29 14:27:07 +0200

    feat: add excepted properties

--- tests/Api/User/UserGetCest.php
+++ tests/Api/User/UserGetCest.php
@@ -4,4 +4,12 @@ namespace App\Tests\Api\User;
 
 class UserGetCest
 {
+    protected static function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'login' => 'string',
+            'roles' => 'array',
+        ];
+    }
 }
