0c7130644abd967ce89c34664ca64f75729da75d
Erwann Maton <erwann.mutant@gmail.com>
2023-10-02 11:35:53 +0200

    test: anonymous can't put other user valid

--- tests/Api/User/UserPutCest.php
+++ tests/Api/User/UserPutCest.php
@@ -2,6 +2,10 @@
 
 namespace App\Tests\Api\User;
 
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
+
 class UserPutCest
 {
     protected static function expectedProperties(): array
@@ -11,5 +15,17 @@ class UserPutCest
             'login' => 'string',
             'roles' => 'array',
         ];
+    }
+
+    public function anonymousUserForbiddenToPutUser(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        UserFactory::createOne();
+
+        // 2. 'Act'
+        $I->sendPut('/api/users/1');
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
     }
 }
