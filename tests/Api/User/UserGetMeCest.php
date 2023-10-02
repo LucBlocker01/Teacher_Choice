094fdbb59cb56083be907b86f07fc31fb1a09089
Erwann Maton <erwann.mutant@gmail.com>
2023-10-02 10:17:03 +0200

    test: authenticated user test on apiMe

--- tests/Api/User/UserGetMeCest.php
+++ tests/Api/User/UserGetMeCest.php
@@ -2,6 +2,10 @@
 
 namespace App\Tests\Api\User;
 
+use App\Entity\User;
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+
 class UserGetMeCest
 {
     protected static function expectedProperties(): array
@@ -13,10 +17,27 @@ class UserGetMeCest
             'roles' => 'array',
             'firstname' => 'string',
             'mail' => 'string',
-            'phones' => 'string',
+            'phone' => 'string',
             'postcode' => 'string',
             'city' => 'string',
-            'adress' => 'string',
+            'address' => 'string',
         ];
+    }
+
+    public function authenticatedUserOnMeGetData(ApiTester $I): void
+    {
+        // 1. 'Arrange'
+        $user = UserFactory::createOne()->object();
+        UserFactory::createOne();
+        $I->amLoggedInAs($user);
+
+        // 2. 'Act'
+        $I->sendGet('/api/me');
+
+        // 3. 'Assert'
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(User::class, '/api/me');
+        $I->seeResponseIsAnItem(self::expectedProperties(), ['login' => $user->getLogin()]);
     }
 }
