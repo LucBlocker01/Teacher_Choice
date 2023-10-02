3f9aca09d04064e61c637af391f1afa422775a3a
Baptiste Clochette <baptiste.clochette@etudiant.univ-reims.fr>
2023-10-02 13:46:54 +0200

    feat: creation of fake data for LessonType

--- src/DataFixtures/LessonTypeFixtures.php
+++ src/DataFixtures/LessonTypeFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\LessonTypeFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,9 @@ class LessonTypeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        LessonTypeFactory::createOne(['name' => 'CM']);
+        LessonTypeFactory::createOne(['name' => 'TD']);
+        LessonTypeFactory::createOne(['name' => 'TDM']);
+        LessonTypeFactory::createOne(['name' => 'TP']);
     }
 }
