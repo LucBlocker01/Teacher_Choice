b42208a05775a0c4ac4c832a0f19e1eadbcabb44
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-03 11:15:05 +0200

    feat: config Fixtures for LessonInformation Entity

--- src/DataFixtures/LessonInformationFixtures.php
+++ src/DataFixtures/LessonInformationFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\LessonFactory;
+use App\Factory\LessonInformationFactory;
+use App\Factory\LessonTypeFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +12,34 @@ class LessonInformationFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        $lessons = LessonFactory::all();
+        $types = LessonTypeFactory::all();
+        foreach ($lessons as $lesson) {
+            $usedtype = [];
+            for ($i = 0; $i == rand(1, 4); ++$i) {
+                $type = $types[rand(1, 4)];
+                // check if the selected type for the given lesson has already been generated
+                while (in_array($type, $usedtype)) {
+                    $type = $types[rand(1, 4)];
+                }
+                // add type generated in the list of used types
+                $usedtype[] = $type;
+                switch ($type) {
+                    case 'CM' == $type->getName():
+                        $nbGroup = 1;
+                        break;
+                    case 'TD' == $type->getName() || 'TDM' == $type->getName():
+                        $nbGroup = rand(1, 4);
+                        break;
+                    case 'TP' == $type->getName():
+                        $nbGroup = rand(2, 8);
+                }
+                LessonInformationFactory::createOne([
+                    'lesson' => $lesson,
+                    'lessonType' => $type,
+                    'nbGroups' => $nbGroup,
+                ]);
+            }
+        }
     }
 }
