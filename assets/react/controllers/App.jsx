8572db1bebdebfb57c41a164e03a154b253a99e9
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-04 10:46:56 +0200

    fix: remove theme import to Header

--- assets/react/controllers/App.jsx
+++ assets/react/controllers/App.jsx
@@ -7,7 +7,7 @@ function App() {
     const theme = createTheme(Normal);
     return (
         <ThemeProvider theme={theme}>
-            <Header theme={theme}></Header>
+            <Header></Header>
             <Button>Emettre ses voeux</Button>
             <Button>Consulter son EDT</Button>
         </ThemeProvider>
