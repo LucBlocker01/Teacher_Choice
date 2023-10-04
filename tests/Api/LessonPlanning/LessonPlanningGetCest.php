e6bffd44c0637ccbf69c4043979384ae15605eca
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-04 14:14:28 +0200

    test: add anonymous user get test for LessonPlanning

--- tests/Api/LessonPlanning/LessonPlanningGetCest.php
+++ tests/Api/LessonPlanning/LessonPlanningGetCest.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace App\Tests\Api\LessonPlanning;
 
+use App\Entity\LessonPlanning;
+use App\Factory\LessonFactory;
+use App\Factory\LessonInformationFactory;
+use App\Factory\LessonPlanningFactory;
+use App\Factory\LessonTypeFactory;
+use App\Factory\SemesterFactory;
+use App\Factory\WeekFactory;
+use App\Factory\WeekStatusFactory;
+use App\Tests\Support\ApiTester;
+
 class LessonPlanningGetCest
 {
     protected function expectedProperties(): array
@@ -14,5 +24,28 @@ class LessonPlanningGetCest
             'information' => 'string:path',
             'weekStatus' => 'string:path',
         ];
+    }
+
+    public function anonymousUserGetLessonPlanning(ApiTester $I): void
+    {
+        SemesterFactory::createOne();
+        WeekFactory::createOne();
+        WeekStatusFactory::createMany(3);
+        LessonTypeFactory::createMany(3);
+        LessonFactory::createOne([
+            'name' => 'test_lesson',
+        ]);
+        LessonInformationFactory::createMany(5);
+        $data = [
+            'nbHours' => 999,
+        ];
+        LessonPlanningFactory::createOne($data);
+
+        $I->sendGet('api/lesson_plannings/1');
+
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(LessonPlanning::class, '/api/lesson_plannings/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), $data);
     }
 }
