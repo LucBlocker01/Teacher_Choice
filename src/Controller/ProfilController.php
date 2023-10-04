de8760ee02d0d5e203f513701a30453db6a7a564
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-04 10:18:50 +0200

    feat: make show route in profil controller

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    #[Route('/profil/{id}', name: 'app_profil_show', requirements: ['id' => '\d+'])]
+    public function show(User $user): Response
     {
-        return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+        $connectedUser = $this->getUser();
+        // security, if user is not connected, redirect to login page
+        if (null === $connectedUser) {
+            return $this->redirectToRoute('app_login');
+        } elseif ($user->getId() !== $connectedUser->getId()) {
+            // if user is not the same as the connected user, redirect to his profil page
+            return $this->redirectToRoute('app_profil_show', ['id' => $this->getUser()->getId()]);
+        }
+
+        return $this->render('profil/show.html.twig', [
+            'user' => $user,
         ]);
     }
 }
