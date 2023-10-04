f2b10d5d5448b3b03367c7c14f7a6c2144225d2a
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-04 14:50:42 +0200

    test: add anonymous user get test for Status

--- tests/Api/Status/StatusGetCest.php
+++ tests/Api/Status/StatusGetCest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Tests\Api\Status;
 
+use App\Entity\Status;
+use App\Factory\StatusFactory;
+use App\Tests\Support\ApiTester;
+
 class StatusGetCest
 {
     protected function expectedProperties(): array
@@ -15,5 +19,22 @@ class StatusGetCest
             'maxHours' => 'float',
             'users' => 'array',
         ];
+    }
+
+    public function anonymousUserGetStatus(ApiTester $I): void
+    {
+        $data = [
+            'name' => 'totallyanadmin',
+            'minHours' => 4.4,
+            'maxHours' => 5.78,
+        ];
+        StatusFactory::createOne($data);
+
+        $I->sendGet('api/statuses/1');
+
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Status::class, '/api/statuses/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), $data);
     }
 }
