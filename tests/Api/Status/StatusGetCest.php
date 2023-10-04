7f14843bb4035a77c0d2d9925fe6e4e0f3fb7e0e
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-04 14:51:00 +0200

    test: add authenticated user get test for Status

--- tests/Api/Status/StatusGetCest.php
+++ tests/Api/Status/StatusGetCest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Api\Status;
 
 use App\Entity\Status;
 use App\Factory\StatusFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ApiTester;
 
 class StatusGetCest
@@ -29,6 +30,25 @@ class StatusGetCest
             'maxHours' => 5.78,
         ];
         StatusFactory::createOne($data);
+
+        $I->sendGet('api/statuses/1');
+
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Status::class, '/api/statuses/1');
+        $I->seeResponseIsAnItem(self::expectedProperties(), $data);
+    }
+
+    public function authenticatedUserGetStatus(ApiTester $I): void
+    {
+        $data = [
+            'name' => 'test',
+            'minHours' => 4.4,
+            'maxHours' => 5.78,
+        ];
+        StatusFactory::createOne($data);
+        $user = UserFactory::createOne()->object();
+        $I->amLoggedInAs($user);
 
         $I->sendGet('api/statuses/1');
 
