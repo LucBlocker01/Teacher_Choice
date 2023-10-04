9d20783686743367c1fc9b60cef1b34a7707484c
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-04 10:31:11 +0200

    feat: import Normal theme

--- assets/react/controllers/App.jsx
+++ assets/react/controllers/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
-import {Button} from "@mui/material";
+import {Button, createTheme, ThemeProvider} from "@mui/material";
+import {Normal} from "../themes/Normal";
 
 function App() {
+    const theme = createTheme(Normal);
     return (
-        <>
-            <Header></Header>
+        <ThemeProvider theme={theme}>
+            <Header theme={theme}></Header>
             <Button>Emettre ses voeux</Button>
             <Button>Consulter son EDT</Button>
-        </>
+        </ThemeProvider>
     );
 }
 
