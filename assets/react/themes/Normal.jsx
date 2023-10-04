7fd582ec0f55c2945a037781ebcc50cf9a8c6e09
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-04 10:15:49 +0200

    feat: add Normal theme

---
+++ assets/react/themes/Normal.jsx
@@ -0,0 +1,13 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const Normal = {
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#B37BB7',
+        },
+        secondary: {
+            main: '#E6D1DD',
+        },
+    },
+};
