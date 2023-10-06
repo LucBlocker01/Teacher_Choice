4eb4188ceecb250cb01f758aa9a792aed1305670
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-06 11:25:13 +0200

    test: test for patch, user can patch own choice

--- tests/Api/Choice/ChoicePatchCest.php
+++ tests/Api/Choice/ChoicePatchCest.php
@@ -43,4 +43,31 @@ class ChoicePatchCest
         ]);
         $i->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
     }
+
+    public function patchOwnChoice(ApiTester $i): void
+    {
+        // Generate data
+        // Generate User
+        StatusFactory::createMany(4);
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER']])->object();
+        $i->amLoggedInAs($user);
+
+        // Generate lesson
+        SemesterFactory::createOne();
+        WeekFactory::createMany(5);
+        WeekStatusFactory::createMany(5);
+        SubjectFactory::createOne();
+        LessonFactory::createOne([
+            'name' => 'Maths',
+        ]);
+        LessonTypeFactory::createMany(5);
+        LessonInformationFactory::createMany(5);
+        LessonPlanningFactory::createMany(5);
+        ChoiceFactory::createOne();
+
+        $i->sendPatch('/api/choices/1', [
+            'nbGroupSelected' => 3,
+        ]);
+        $i->seeResponseCodeIs(HttpCode::OK);
+    }
 }
