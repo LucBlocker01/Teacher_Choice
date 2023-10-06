4d2a746b35e60cad59aeab935754150e8ef003b1
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-10-06 14:36:02 +0200

    fix: Modify Choices with ChoicesList component

--- assets/react/components/Choices.jsx
+++ assets/react/components/Choices.jsx
@@ -1,34 +1,31 @@
 import React from "react";
-import {Button} from "@mui/material";
-import Stack from '@mui/material/Stack';
+import {Button, Container} from "@mui/material";
+import ChoicesList from "./ChoicesList";
 
 
 function Choices() {
     return (
         <>
-            <Stack spacing={2} sx={{
+            <Container sx={{
                 display: "flex",
                 alignItems: "center",
-                justifyContent: "center",
-                height: "100%",
-                border: 1,
-                backgroundColor: "primary.main",
-                marginBottom: 2,
+                justifyContent: "space-around",
+                flexWrap: "wrap",
             }}>
-                <div>item</div>
-            </Stack>
-            <Button sx={{
-                border: 1,
-                backgroundColor: "secondary.main",
-            }}>
-                Ajouter voeux
-            </Button>
-            <Button sx={{
-                border: 1,
-                backgroundColor: "secondary.main",
-            }}>
-                Valider voeux
-            </Button>
+                <ChoicesList />
+                <Button sx={{
+                    border: 1,
+                    backgroundColor: "secondary.main",
+                }}>
+                    Ajouter voeux
+                </Button>
+                <Button sx={{
+                    border: 1,
+                    backgroundColor: "secondary.main",
+                }}>
+                    Valider voeux
+                </Button>
+            </Container>
         </>
     );
 }
