c44a03e6f778f9e376fe64c4f373a8e560cddd27
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-06 11:04:18 +0200

    test: delete test for other user choice

--- tests/Api/Choice/ChoiceDeleteCest.php
+++ tests/Api/Choice/ChoiceDeleteCest.php
@@ -66,4 +66,31 @@ class ChoiceDeleteCest
         $i->sendDelete('/api/choices/1');
         $i->seeResponseCodeIs(HttpCode::NO_CONTENT);
     }
+
+    public static function deleteOtherChoice(ApiTester $i): void
+    {
+        // Generate data
+        // Generate User
+        StatusFactory::createMany(4);
+        UserFactory::createOne();
+
+        // Generate lesson
+        SemesterFactory::createOne();
+        WeekFactory::createMany(5);
+        WeekStatusFactory::createMany(5);
+        SubjectFactory::createOne();
+        LessonFactory::createOne([
+            'name' => 'Maths',
+        ]);
+        LessonTypeFactory::createMany(5);
+        LessonInformationFactory::createMany(5);
+        LessonPlanningFactory::createMany(5);
+        ChoiceFactory::createOne();
+
+        $user = UserFactory::createOne(['roles' => ['ROLE_ADMIN']])->object();
+        $i->amLoggedInAs($user);
+
+        $i->sendDelete('/api/choices/1');
+        $i->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
 }
