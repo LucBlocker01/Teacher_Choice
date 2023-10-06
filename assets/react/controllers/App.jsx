108685c2f51aa8512e9e8613a8babae9b0e8cd36
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-10-06 10:50:34 +0200

    feat: add Choices component

--- assets/react/controllers/App.jsx
+++ assets/react/controllers/App.jsx
@@ -4,6 +4,7 @@ import {createTheme, ThemeProvider} from "@mui/material";
 import {Normal} from "../themes/Normal";
 import Home from "../components/Home";
 import {Route, Router} from "wouter";
+import Choices from "../components/Choices";
 
 function App() {
     const theme = createTheme(Normal);
