faccd7167432ad5efd5754e480670eaf2dc40141
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-10-06 15:18:23 +0200

    feat: add useEffect for add item for each choice

--- assets/react/components/ChoicesList.jsx
+++ assets/react/components/ChoicesList.jsx
@@ -1,10 +1,22 @@
 import React, {useEffect, useState} from "react";
+import {Stack} from "@mui/material";
+import {fetchMyChoice} from "../services/api/api";
+
 
 function ChoicesList() {
     const [ ChoiceList, setChoiceList ] = useState() ;
 
+    useEffect(() => {
+        fetchMyChoice().then((data) => {
+            setChoiceList(
+                data["hydra:member"].map((choice) => (
+                    <ChoiceItem data={choice}></ChoiceItem>
+                ))
+            );
+        });
+    }, []);
+
     return (
-        <div></div>
     );
 }
 
