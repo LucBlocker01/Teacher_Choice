7ec447dd30c4bde80c998cb0c39154bd26be5956
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-06 10:43:53 +0200

    test: add test for classic user can post choice

--- tests/Api/Choice/ChoicePostCest.php
+++ tests/Api/Choice/ChoicePostCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Api\Choice;
 
+use App\Entity\Choice;
 use App\Factory\LessonFactory;
 use App\Factory\LessonInformationFactory;
 use App\Factory\LessonPlanningFactory;
@@ -74,5 +75,37 @@ class ChoicePostCest
             'lessonInformation' => '/api/lesson_informations/1',
         ]);
         $i->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
+
+    public static function postChoiceUserIsRoleUser(ApiTester $i): void
+    {
+        // Generate data
+        // Generate User
+        StatusFactory::createMany(4);
+        $user = UserFactory::createOne(['roles' => ['ROLE_USER']])->object();
+        $i->amLoggedInAs($user);
+        $otherUser = UserFactory::createOne()->object();
+        // Generate lesson
+        SemesterFactory::createOne();
+        WeekFactory::createMany(5);
+        WeekStatusFactory::createMany(5);
+        SubjectFactory::createOne();
+        LessonFactory::createOne([
+            'name' => 'Maths',
+        ]);
+        LessonTypeFactory::createMany(5);
+        LessonInformationFactory::createMany(5);
+        LessonPlanningFactory::createMany(5);
+
+        $i->sendPost('/api/choices', [
+            'nbGroupSelected' => 4,
+            'year' => '2023',
+            'teacher' => '/api/users/1',
+            'nbGroupAttributed' => 0,
+            'lessonInformation' => '/api/lesson_informations/1',
+        ]);
+        $i->seeResponseCodeIs(HttpCode::CREATED);
+        $i->seeResponseIsJson();
+        $i->seeResponseIsAnEntity(Choice::class, '/api/choices/1');
     }
 }
