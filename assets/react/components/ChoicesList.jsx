0b3aa46360ec8125082e2fc9a2a27342def25b93
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-10-06 14:32:56 +0200

    feat: creation of ChoicesList Component for Choices

---
+++ assets/react/components/ChoicesList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Container} from "@mui/material";
+
+
+function ChoicesList() {
+    return (
+        <Container sx={{
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "primary.main",
+            border: 1,
+            marginBottom: 2,
+            borderRadius: "5px",
+        }}>
+            ds
+        </Container>
+    );
+}
+
+export default ChoicesList;
