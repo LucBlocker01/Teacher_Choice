ddc999d56d6a9f824c39e091b91905aa26df88a9
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-10-23 12:02:41 +0200

    feat: show choices informations on list in process

--- assets/react/components/ChoiceItem.jsx
+++ assets/react/components/ChoiceItem.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Button, Container} from "@mui/material";
+import { fetchByApiUrl } from "../services/api/api";
 function ChoiceItem({ data }) {
-    // data -> nbGroupSelected, year, lessonInformation
+    // data -> id, nbGroupSelected, year, lessonInformation
+
+    const [ lessonInformation, setLessonInformation ] = useState({}) ;
+    const [ lesson, setLesson ] = useState({}) ;
+
+    useEffect(() => {
+        fetchByApiUrl(data.lessonInformation).then((dataInfo) => setLessonInformation(dataInfo))
+    }, [data]);
+
+    useEffect(() => {
+        if (lessonInformation.lesson !== undefined) {
+            console.log(lessonInformation.lesson);
+            fetchByApiUrl(lessonInformation.lesson).then((dataLesson) => setLesson(dataLesson))
+        }
+    }, [lessonInformation]);
+
+    // console.log(lessonInformation);
+    console.log(lesson);
     return (
-        <div>test</div>
+        <Container id={data.id} sx={{
+            backgroundColor: "secondary.main",
+            margin: "1%"
+        }}>
+            <div>{data.year}</div>
+            <div>{data.nbGroupSelected}</div>
+            <div>{lessonInformation.id}</div>
+            <Button sx={{
+                border: 1,
+                backgroundColor: "primary.main",
+                color: "secondary.main",
+                borderColor: "secondary.main"
+            }}>Modifier</Button>
+        </Container>
     );
 }
 
