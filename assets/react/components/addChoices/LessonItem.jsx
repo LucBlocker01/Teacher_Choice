45ffe9d507a710a6438a63d9949f25bbb1c09a5d
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-23 17:17:39 +0200

    feat: beggining of showing lesson for each subject

---
+++ assets/react/components/addChoices/LessonItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function LessonItem() {
+  return (
+    // possiblement mettre un form
+    <p>Name, Type, nbGroup  checkbox   button choisir</p>
+  )
+}
+
+export default LessonItem
--- assets/react/components/addChoices/SubjectItem.jsx
+++ assets/react/components/addChoices/SubjectItem.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { fetchSubjectBySemester } from "../../services/api/choice";
+import LessonItem from "./LessonItem";
 
 function SubjectItem({ data }) {
+
+    const [lessons, setLessons] = useState(null);
+
+    const SubjectClick = () => {
+        console.log(data.id);
+        //fetchLessonBySubject a faire en back
+        fetchSubjectBySemester(1).then((data) => {
+            setLessons(data["hydra:member"].map((lesson) => (
+                <LessonItem key={lesson.id} data={lesson} />
+            )));
+        });
+    }
+
   return (
-    <p>{data.name}</p>
+    <div onClick={SubjectClick}>{data.name}
+        {lessons}
+    </div>
   )
 }
 
