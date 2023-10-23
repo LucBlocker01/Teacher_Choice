b73b0220736ac96efbf22c3447af34fe22b50947
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-23 16:25:29 +0200

    feat: add a fetchSubjectBySemester function

--- assets/react/services/api/choice.js
+++ assets/react/services/api/choice.js
@@ -3,3 +3,7 @@ export const BASE_URL = '127.0.0.1:8000/';
 export function fetchSemesters() {
     return fetch('/api/semesters').then(response => response.json());
 }
+
+export function fetchSubjectBySemester(id) {
+    return fetch(`/api/subjects/semester/${id}`).then(response => response.json());
+}
