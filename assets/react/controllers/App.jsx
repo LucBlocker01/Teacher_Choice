60368dfa940c22921cc1a505fc39323c4dc6a9b1
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-24 10:10:25 +0200

    feat: add CssBaseline

--- assets/react/controllers/App.jsx
+++ assets/react/controllers/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Header from "../components/Header";
 import {createTheme, ThemeProvider} from "@mui/material";
 import {Normal} from "../themes/Normal";
+import {Dark} from "../themes/Dark";
 import Home from "../components/Home";
 import {Route, Router} from "wouter";
 import Choices from "../components/Choices";
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
     const theme = createTheme(Normal);
     return (
         <ThemeProvider theme={theme}>
+            <CssBaseline />
             <Header></Header>
             <Router>
                 <Route path="/react" component={Home}/>
