e2279239c6ab01aba43965ea6ca8039aaf7bab77
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-24 15:41:12 +0200

    feat: change useGetSemesters hook

--- assets/react/hooks/useGetSemesters.js
+++ assets/react/hooks/useGetSemesters.js
@@ -1,14 +1,10 @@
 
 
-function useGetSemesters(semester, setSemesterData) {
-    fetch("api/semesters")
+function useGetSemesters() {
+    return fetch("/api/semesters")
         .then((response) => {
             return response.json()
-        }).then((data) => {
-            setSemesterData(data);
-        }
-    )
-    return semester;
+        })
 }
 
 export default useGetSemesters;
