264c5296cb4cd64a0ffbdf8bf8234641d91befb9
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-24 17:44:05 +0200

    feat: can post a choice

--- assets/react/components/addChoices/LessonItem.jsx
+++ assets/react/components/addChoices/LessonItem.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import useGetMe from "../../hooks/useGetMe";
+import {postChoice} from "../../services/api/choice";
 
-function LessonItem() {
+function LessonItem({data}) {
+
+    const [user, setUser] = useState(useGetMe(user, setUser));
+
+    //User
+    useEffect(() => {
+        useGetMe().then((data) => {
+            setUser(data);
+        })
+    }, [])
+    const submitChoice = (event) => {
+        event.preventDefault();
+        const year = new Date().getFullYear().toString();
+        const lessonInformation = data["@id"];
+        const nbGroupSelectedTemp = event.target[0].value;
+        const nbGroupSelected = parseInt(nbGroupSelectedTemp);
+        const teacher = "/api/users/"+event.target[1].value;
+
+        //verif valeur au dessus du nombre possible et inférieur à 0 ou 0
+        const dataPost = {
+            teacher,
+            lessonInformation,
+            nbGroupSelected,
+            year
+        }
+        postChoice(dataPost)
+        console.log({teacher, lessonInformation, nbGroupSelected, year});
+  }
+
   return (
-    // possiblement mettre un form
-    <p>Name, Type, nbGroup  checkbox   button choisir</p>
+    <div>
+      {data.lessonType.name}
+      <form onSubmit={submitChoice}>
+        <input name='nbGroupe' type='number' placeholder='1'/>
+        <input name='userID' type='hidden' value={user.id} />
+        <button type='submit'>Choisir</button>
+      </form>
+      </div>
   )
 }
 
