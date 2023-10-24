08cf12ebcf5786fbee81e868dc83d9d29ffa2672
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-24 11:06:10 +0200

    feat: add useTheme hook

---
+++ assets/react/hooks/useTheme.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from "react"
+import {createTheme, useMediaQuery} from "@mui/material";
+import {Normal} from "../themes/Normal";
+import {Dark} from "../themes/Dark";
+
+function useTheme() {
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+    const [isNormal, setIsNormal] = useState(true);
+    let [theme, setTheme] = useState(
+        createTheme(Normal)
+    );
+
+    function toggleTheme() {
+        setIsNormal(!isNormal);
+        setTheme(
+            createTheme(
+                isNormal ? Normal : Dark
+            )
+        )
+        useEffect(() => {
+            if (prefersDarkMode) {
+                toggleTheme();
+            }
+        }, []);
+    }
+
+    return {isNormal, theme, toggleTheme}
+}
