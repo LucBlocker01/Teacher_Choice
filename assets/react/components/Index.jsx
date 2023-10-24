3f70199ffd559edb7e39b844d9bf4680c62f0437
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-24 14:54:19 +0200

    feat: add h1 title to Index

--- assets/react/components/Index.jsx
+++ assets/react/components/Index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import {Box} from "@mui/material";
 
 function Index() {
     return(
-        <p>test</p>
+        <Box sx={{
+            mb: "100px",
+        }}>
+            <img className="logo" src="{{ asset('/img/urca.png') }}" alt="" />
+            <h1 className="title">Liste des matières par semestre</h1>
+
+        </Box>
     )
 }
 
