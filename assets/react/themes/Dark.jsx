ed91d54c27c449f85d47291368a1f4ba373aa5c8
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-24 10:04:45 +0200

    feat: add Dark Mode

---
+++ assets/react/themes/Dark.jsx
@@ -0,0 +1,14 @@
+export const Dark = {
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#B37BB7',
+        },
+        secondary: {
+            main: '#E6D1DD',
+        },
+        accent: {
+            main: '#97537A'
+        }
+    },
+};
