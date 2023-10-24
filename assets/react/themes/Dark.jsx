2dd6e890d25fb4f8743399e74a67ff0fd837c665
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-10-24 10:45:47 +0200

    feat: overwrite css background property for body tag
    This is done in order to change the background dots color
    to be gray instead of white in a pitch black background

--- assets/react/themes/Dark.jsx
+++ assets/react/themes/Dark.jsx
@@ -1,3 +1,5 @@
+const dotsColor = '#383838';
+
 export const Dark = {
     palette: {
         mode: 'dark',
@@ -10,5 +12,12 @@ export const Dark = {
         accent: {
             main: '#97537A'
         }
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: (themeParam) => ({
+                body: themeParam.palette.mode === 'dark' ? {backgroundImage: `radial-gradient(circle, ${dotsColor} 10%, transparent 11%),radial-gradient(circle at bottom left, ${dotsColor} 5%, transparent 6%),radial-gradient(circle at bottom right, ${dotsColor} 5%, transparent 6%),radial-gradient(circle at top left, ${dotsColor} 5%, transparent 6%),radial-gradient(circle at top right, ${dotsColor} 5%, transparent 6%)`} : null,
+}),
+        },
     },
 };
