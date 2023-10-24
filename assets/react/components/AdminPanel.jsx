306439f2e435a00d35a3bdcf64dc0cb455a2db12
Erwann Maton <erwann.mutant@gmail.com>
2023-10-24 11:40:09 +0200

    feat: Do research field

---
+++ assets/react/components/AdminPanel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {TableContainer} from "@mui/material";
+
+
+function PlanningPanel() {
+
+    return (
+       <TableContainer>
+
+       </TableContainer>
+    );
+}
+
+export default PlanningPanel;
