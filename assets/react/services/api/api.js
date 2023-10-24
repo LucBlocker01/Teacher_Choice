1bfd83e7e7eb7c315958b1272dd12e5002ae179e
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-10-24 10:10:31 +0200

    feat: add delete choice call to api

--- assets/react/services/api/api.js
+++ assets/react/services/api/api.js
@@ -5,3 +5,7 @@ export function fetchMyChoice(){
 export function fetchByApiUrl(urlApi){
     return fetch(`${urlApi}`).then((response) => response.json());
 }
+
+export function deleteChoiceById(id){
+    return fetch(`/api/choices/${id}`, {method: 'DELETE'}).then((response) => response.json());
+}
