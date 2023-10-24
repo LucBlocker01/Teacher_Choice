ca13e340deeb7aed68b3b3d79768f3a48d9fdf10
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-10-24 17:44:06 +0200

    feat: add modify on choice list

--- assets/react/components/ChoiceItem.jsx
+++ assets/react/components/ChoiceItem.jsx
@@ -3,9 +3,9 @@ import {
     Box,
     Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
     TableCell,
-    TableRow
+    TableRow, TextField
 } from "@mui/material";
-import {deleteChoiceById} from "../services/api/api";
+import {deleteChoiceById, modifyChoiceById} from "../services/api/api";
 
 function ChoiceItem({ data }) {
     // data -> id, nbGroupSelected, year, lessonInformation
@@ -38,27 +38,42 @@ function ChoiceItem({ data }) {
         }
     }, [lessonInformation]);*/
 
-    const [open, setOpen] = React.useState(false);
+    const [openDelete, setOpenDelete] = React.useState(false);
+    const [openEdit, setOpenEdit] = React.useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
+    const handleClickOpenDelete = () => {
+        setOpenDelete(true);
     };
 
-    const handleClose = () => {
-        setOpen(false);
+    const handleCloseDelete = () => {
+        setOpenDelete(false);
     };
 
-    const handleAccept = () => {
-        setOpen(false);
+    const handleAcceptDelete = () => {
+        setOpenDelete(false);
         deleteChoiceById(data.id).then();
+        location.reload();
+    };
+
+    const handleClickOpenEdit = () => {
+        setOpenEdit(true);
+    };
+
+    const handleCloseEdit = () => {
+        setOpenEdit(false);
+    };
+
+    const handleAcceptEdit = () => {
+        setOpenEdit(false);
+        modifyChoiceById(data.id, document.getElementById('nbGroupSelected').value).then();
         location.reload();
     };
 
     return (
         <>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+            <Dialog open={openDelete} onClose={handleCloseDelete} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
                 <DialogTitle id="alert-dialog-title">
-                    {"Supperssion"}
+                    {"Suppression"}
                 </DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-description">
@@ -66,12 +81,40 @@ function ChoiceItem({ data }) {
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Ne pas supprimer</Button>
-                    <Button onClick={handleAccept} autoFocus>
+                    <Button onClick={handleCloseDelete}>Ne pas supprimer</Button>
+                    <Button onClick={handleAcceptDelete} autoFocus>
                         Supprimer
                     </Button>
                 </DialogActions>
             </Dialog>
+
+            <Dialog open={openEdit} onClose={handleCloseEdit} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+                <DialogTitle id="alert-dialog-title">
+                    {"Modification"}
+                </DialogTitle>
+                <DialogContent sx={{ textAlign: "center" }}>
+                    <DialogContentText id="alert-dialog-description" sx={{ marginBottom: "5%" }}>
+                        Placer ici le nouveau nombre de groupes a encadrer pour ce {data.lessonInformation.lessonType.name} de {data.lessonInformation.lesson.name}
+                    </DialogContentText>
+                    <TextField
+                        id="nbGroupSelected"
+                        type="number"
+                        autoFocus
+                        fullWidth
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        InputProps={{ inputProps: { min: 0, max: data.lessonInformation.nbGroups } }}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseEdit}>Ne pas modifier</Button>
+                    <Button onClick={handleAcceptEdit} autoFocus>
+                        Modifier
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
             <TableRow>
                 <TableCell component="th" scope="row">{data.lessonInformation.lesson.name}</TableCell>
                 <TableCell align="right">{data.lessonInformation.lesson.subject.semester.name}</TableCell>
@@ -90,11 +133,13 @@ function ChoiceItem({ data }) {
                 </TableCell>
                 <TableCell align="right">{data.lessonInformation.lessonType.name}</TableCell>
                 <TableCell>
-                    <Button sx={{ border: 1 }}>Modifier</Button>
+                    <Button sx={{ border: 1 }} onClick={() => {
+                        handleClickOpenEdit();
+                    }} >Modifier</Button>
                 </TableCell>
                 <TableCell>
                     <Button sx={{ border: 1 }} onClick={() => {
-                        handleClickOpen();
+                        handleClickOpenDelete();
                     }}>Supprimer</Button>
                 </TableCell>
             </TableRow>
