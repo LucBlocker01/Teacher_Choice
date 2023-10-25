a14901df04907de867ef9e0c3f75bac83f7a62d7
Alban Robion <alban.robion@etudiant.univ-reims.fr>
2023-10-25 13:56:45 +0200

    fix: weekly task in process

--- assets/react/components/WeeklyTask.jsx
+++ assets/react/components/WeeklyTask.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {fetchAllWeeks, fetchMyChoice} from "../services/api/api";
+import Paper from "@mui/material/Paper";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 
 function WeeklyTask(){
     const [ weeklyTask, setWeeklyTask ] = useState({}) ;
@@ -15,20 +17,51 @@ function WeeklyTask(){
         } )
     }, []);
 
-    console.log(weeklyTask);
-
     useEffect(() => {
         fetchMyChoice().then((data) => {
             data["hydra:member"].map((choice) => {
                 choice.lessonInformation.lessonPlannings.map((lessonPlanning) => {
-
+                    objectWeek[lessonPlanning.weekStatus.week.weekNum] += lessonPlanning.nbHours
+                    setWeeklyTask(objectWeek);
                 })
             })
         });
     }, []);
 
-    return (<>
+    console.log(weeklyTask);
 
+    return (<>
+        <TableContainer sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            backgroundColor: "secondary.main",
+            border: 1,
+            marginBottom: 2,
+            borderRadius: "5px",
+            overflowX: "auto",
+            overflowY: "auto",
+            maxHeight: "500px",
+            borderColor: "primary.main"
+        }} component={Paper}>
+            <Table sx={{
+                minWidth: 800,
+            }} size="small" aria-label="simple table">
+                <TableHead sx={{
+                    backgroundColor: "primary.main",
+                    position:"sticky",
+                    top: 0,
+                }}>
+                    <TableRow>
+
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
     </>);
 }
 
