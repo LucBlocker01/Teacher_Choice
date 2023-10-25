09c191fd8b022a9e868d902ebd82f97c10f8766b
Baptiste Clochette <baptiste.clochette@etudiant.univ-reims.fr>
2023-10-25 11:36:14 +0200

    feat: creation of constructor of ExcelManager entity

--- src/Utils/ExcelManager.php
+++ src/Utils/ExcelManager.php
@@ -1,9 +1,17 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Utils;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class ExcelManager
 {
+    private EntityManagerInterface $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 }
