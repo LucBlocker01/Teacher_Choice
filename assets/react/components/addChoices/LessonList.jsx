d6b4fd77de5db361066eccf63928986d23b20130
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-26 11:47:51 +0200

    feat: add acorrdion

--- assets/react/components/addChoices/LessonList.jsx
+++ assets/react/components/addChoices/LessonList.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
 import LessonItem from './LessonItem';
+import {Accordion, AccordionDetails, AccordionSummary} from "@mui/material";
+import {ExpandMore} from "@mui/icons-material";
 
-function LessonList({data}) {
+function LessonList({data, MR, user}) {
 
     const [lessonsInfo, setLessonsInfo] = useState(null);
+    const [expanded, setExpanded] = useState(false);
 
     const LessonClick = () => {
         setLessonsInfo(data["lessonInformation"].map((lessonInfo) => {
-            return <LessonItem key={lessonInfo.id} data={lessonInfo} />
+            return <LessonItem key={lessonInfo.id} data={lessonInfo} user={user} />
         }));
     }
 
   return (
-    <div>
-        <span onClick={LessonClick}>{data.name}</span>
-        <div>
-            {lessonsInfo}
-            {console.log(lessonsInfo)}
-        </div>
-    </div>
+      <Accordion sx={{margin: "10px"}}>
+          <AccordionSummary expandIcon={<ExpandMore />} onClick={LessonClick}>
+              {MR}&nbsp;{data.name}
+          </AccordionSummary>
+          <AccordionDetails>
+                {lessonsInfo}
+          </AccordionDetails>
+      </Accordion>
   )
 }
 
