1efdd1c850e48b90cffb9f994319ea6d4856d0d6
Erwann Maton <erwann.mutant@gmail.com>
2023-10-27 11:19:58 +0200

    feat: modify patch function

--- assets/react/services/api/api.js
+++ assets/react/services/api/api.js
@@ -25,7 +25,7 @@ export function PatchTeacherChoiceById(id, nbGroupAttributed){
             },
             method: 'PATCH',
             body: JSON.stringify({
-                nbGroupAttributed: nbGroupAttributed
+                nbGroupAttributed: parseInt(nbGroupAttributed)
             })})
         .then((response) => response.json());
 }
