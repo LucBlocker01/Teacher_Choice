8dd7893d942ad8a46431d3079aa5625731eb2a5a
alve0031 <kilian.alves@etudiant.univ-reims.fr>
2023-10-27 14:47:56 +0200

    refactor: remove unused props

--- assets/react/components/addChoices/SubjectItem.jsx
+++ assets/react/components/addChoices/SubjectItem.jsx
@@ -2,16 +2,16 @@ import React, {useEffect, useState} from 'react'
 import { fetchLessonBySubject } from "../../services/api/choice";
 import LessonList from "./LessonList";
 
-function SubjectItem({ data, user, handleChangeAccordion, expanded }) {
+function SubjectItem({ data, user }) {
 
     const [lessons, setLessons] = useState(null);
 
     useEffect(() => {
             const MR = data.name;
             setLessons(data["lessons"].map((lesson) => (
-                <LessonList key={lesson.id} data={lesson} MR={MR} user={user} handleChangeAccordion={handleChangeAccordion} expanded={expanded}/>
+                <LessonList key={lesson.id} data={lesson} MR={MR} user={user} />
             )));
-    }, [user, expanded]);
+    }, [user]);
 
     if (lessons === null) {
         return <div>Loading...</div>;
--- assets/react/components/addChoices/SubjectList.jsx
+++ assets/react/components/addChoices/SubjectList.jsx
@@ -7,11 +7,7 @@ function SubjectList({ data }) {
 
     const [subjectList, setSubjectList] = useState(null);
     const [user, setUser] = useState(null);
-    const [expanded, setExpanded] = useState(false);
 
-    const handleChangeAccordion = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-    }
 
     //useEffect pour récupérer l'utilisateur connecté
     useEffect(() => {
@@ -20,13 +16,12 @@ function SubjectList({ data }) {
             setUser(user);
         };
         userFetch();
-    }, [expanded]);
+    }, []);
 
     useEffect(() => {
-        console.log(user);
         fetchSubjectBySemester(data).then((data) => {
             setSubjectList(data["hydra:member"].map((subject) => (
-                <SubjectItem key={subject.id} data={subject} user={user} handleChangeAccordion={handleChangeAccordion} expanded={expanded} />
+                <SubjectItem key={subject.id} data={subject} user={user} />
             )));
         });
     }, [user]);
