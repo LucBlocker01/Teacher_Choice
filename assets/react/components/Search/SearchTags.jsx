dbbf063134726082c801046f8b630e1b0a77faef
LAPTOP-QMITUAI4\hluca <lucas.hureaux@etudiant.univ-reims.fr>
2023-11-02 17:53:12 +0100

    feat: add SearchTags component

---
+++ assets/react/components/Search/SearchTags.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TextField} from "@mui/material";
+
+function SearchTags() {
+    return (
+        <>
+        <TextField/>
+        </>
+    )
+}
+
+export default SearchTags;
