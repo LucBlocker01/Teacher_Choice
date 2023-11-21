2c0687ed066836587b476c97d170a15300e46da8
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-11-21 14:44:28 +0100

    fix: fix swap theme button not working when in profile page

--- assets/react/controllers/AppBar.jsx
+++ assets/react/controllers/AppBar.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import {Normal} from "../themes/Normal";
 import {createTheme, ThemeProvider} from "@mui/material";
+import useTheme from "../hooks/useTheme";
+import CssBaseline from "@mui/material/CssBaseline";
 function AppBar() {
-    const theme = createTheme(Normal);
+    const {isNormal, theme, toggleTheme} = useTheme();
     return(
         <ThemeProvider theme={theme}>
-            <Header/>
+            <CssBaseline />
+            <Header toggleTheme={toggleTheme} isNormal={isNormal}></Header>
         </ThemeProvider>
     )
 }
