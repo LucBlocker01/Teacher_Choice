4c98fb02d6310a42581fcd8438de8f5b4e086ac8
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2023-11-24 10:58:16 +0100

    fix: fix p inside of p dom error

--- assets/react/components/indexTableContent/IndexRowTable.jsx
+++ assets/react/components/indexTableContent/IndexRowTable.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {fetchSubjectBySemester} from "../../services/api/choice";
-import {TableCell, TableRow, Typography} from "@mui/material";
+import {Box, TableCell, TableRow, Typography} from "@mui/material";
 
 
 function IndexRowTable({data, searchInput}) {
@@ -23,7 +23,7 @@ function IndexRowTable({data, searchInput}) {
                             </TableCell>
                             <TableCell>
                                 {subject.lessons.map((lesson) => (
-                                    <Typography sx={{display: "flex", gap: 1}}>{lesson.tags.map((tag) => <Typography>{tag.name}</Typography>)}</Typography>
+                                    <Box sx={{display: "flex", gap: 1, pl: "0"}}>{lesson.tags.map((tag) => <Typography>{tag.name}</Typography>)}</Box>
                                 )
                                 )}
                             </TableCell>
